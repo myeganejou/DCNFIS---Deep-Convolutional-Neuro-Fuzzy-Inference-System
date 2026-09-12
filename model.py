dc70c3fbbb616997b9db54409c927c0c6006e0de
Mojtaba Yeganejou <yeganejo@engineering.ualberta.ca>
2026-09-12 07:15:47 -0600

    Adding DCNFIS model

---
+++ model.py
@@ -0,0 +1,121 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers as layers
+from tensorflow.keras import models as models
+from tensorflow.keras import backend as K
+
+
+# custom layer for gauss membership function
+class GaussMembership(layers.Layer):
+    
+    def __init__(self, num_rules, epsilon=1e-8, **kwargs):
+        self.epsilon = epsilon
+        self.num_rules = num_rules
+        super(GaussMembership, self).__init__(**kwargs)
+        
+        
+    def build(self, input_shape):
+        self.mu = self.add_weight(
+            name="mu",
+            shape=(self.num_rules, input_shape[1]),
+            initializer=keras.initializers.Zeros(),
+            trainable=True)
+        self.sigma = self.add_weight(
+            name="sigma",
+            shape=(self.num_rules, input_shape[1]),
+            initializer=keras.initializers.Ones(),
+            constraint=keras.constraints.NonNeg(),
+            trainable=True)
+        super(GaussMembership, self).build(input_shape)
+    
+    
+    def get_config(self):
+
+        config = super().get_config().copy()
+        config.update({
+            'epsilon': self.epsilon,
+            'num_rules': self.num_rules,
+        })
+        return config
+    
+    def call(self, x):
+        x = K.expand_dims(x, axis=1)
+        x = K.square((x - self.mu) / (self.sigma + self.epsilon))
+        return K.exp(-0.5 * x)
+    
+    
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.num_rules, input_shape[1])
+    
+    
+class LogGaussMF(layers.Layer):
+    
+    def __init__(
+        self, 
+        rules=10,
+        mu_initializer="zeros",
+        beta_initializer="ones",
+        mu_regularizer=None,
+        beta_regularizer=None,
+        activity_regularizer=None,
+        mu_constraint=None,
+        beta_constraint="non_neg",
+        **kwargs
+    ):
+        super(LogGaussMF, self).__init__(**kwargs)
+        self.rules = int(rules)
+        self.mu_initializer = tf.keras.initializers.get(mu_initializer)
+        self.beta_initializer = tf.keras.initializers.get(beta_initializer)
+        self.mu_regularizer = tf.keras.regularizers.get(mu_regularizer)
+        self.beta_regularizer = tf.keras.regularizers.get(beta_regularizer)
+        self.mu_constraint = tf.keras.constraints.get(mu_constraint)
+        self.beta_constraint = tf.keras.constraints.get(beta_constraint)
+        
+        
+    def get_config(self):
+
+        config = super().get_config().copy()
+        config.update({
+            'rules': self.rules,
+            'mu_initializer': self.mu_initializer,
+            'beta_initializer': self.beta_initializer,
+            'mu_regularizer': self.mu_regularizer,
+            'beta_regularizer': self.beta_regularizer,
+            'mu_constraint': self.mu_constraint,
+            'beta_constraint': self.beta_constraint,
+        })
+        return config
+    
+    
+    def build(self, input_shape):
+        self.mu = self.add_weight(
+            name="mu",
+            shape=(self.rules, input_shape[1]),
+            initializer=self.mu_initializer,
+            regularizer=self.mu_regularizer,
+            trainable=True)
+        self.beta = self.add_weight(
+            name="beta",
+            shape=(self.rules, input_shape[1]),
+            initializer=self.beta_initializer,
+            regularizer=self.beta_regularizer,
+            constraint=self.beta_constraint,
+            trainable=True)
+        super(LogGaussMF, self).build(input_shape)
+    
+    
+    def call(self, x):
+        x = K.expand_dims(x, axis=1)
+        return -0.5 * K.square(x - self.mu) * self.beta
+    
+    
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.rules, input_shape[1])
+    
+    
+# multiplication layer lambda function
+def normalized_product_fn(x):
+    x = tf.keras.ops.sum(K.log(x + 1e-8), axis=2)
+    return tf.keras.ops.exp(x - K.max(x, axis=1, keepdims=True))
+
+
